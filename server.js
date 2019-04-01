dc1ff2f36da25f0c59adffd0fee28e818d611685
Dalton <dschnieders@ksu.edu>
2019-04-01 14:11:12 -0500

    T

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 "use strict";
 
-// PORT definition
-const PORT = 3000;
-
-// Import the HTTP library
+const PORT = 8080;
 const http = require('http');
-
-// Import the fs library
 const fs = require('fs');
 
 const express = require('express');
