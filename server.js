45fb8568f6d2944f075976e7921a9371c38772a4
Dalton <dschnieders@ksu.edu>
2019-04-01 14:09:38 -0500

    Reordered everything

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+"use strict";
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+// PORT definition
+const PORT = 3000;
+
+// Import the HTTP library
+const http = require('http');
+
+// Import the fs library
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
+const cache = {};
+cache['currentPoints.html'] = fs.readFileSync('public/currentPoints.html');
+cache['incentives.html'] = fs.readFileSync('public/incentives.html');
+cache['logPoints.html'] = fs.readFileSync('public/logPoints.html');
+
+app.use(express.static("./public"));
+app.get('/', function(req, res) {
+    res.sendfile('public/index.html');
 });
 
-app.listen(8080);
+app.listen(PORT, function () {
+  console.log('Listing To Port');
+});
